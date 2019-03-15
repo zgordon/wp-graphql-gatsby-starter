c8ea40a4d1e9e3da020769e051361763dd2a279f
alexandra <alexaspalato@gmail.com>
2019-03-15 10:04:49 +0100

    createUsers

---
+++ packages/gatsby-theme-starter/src/templates/users/archive.js
@@ -0,0 +1,30 @@
+import React from "react"
+import Layout from "../../components/layout"
+import PostEntry from "../../components/PostEntry"
+import SEO from "../../components/seo"
+
+const AuthorArchive = props => {
+  const {
+    pageContext: { name, posts },
+  } = props
+  return (
+    <Layout classNames="archive">
+      <SEO
+        title={`Author - ${name}`}
+        description={`A collection of posts written by ${name}.`}
+      />
+      <header className="page-header">
+        <h1 className="page-title">
+          Author Archives: <span className="page-description">{name}</span>
+        </h1>
+      </header>
+
+      {posts.nodes &&
+        posts.nodes.map(post => {
+          return <PostEntry key={post.id} post={post} />
+        })}
+    </Layout>
+  )
+}
+
+export default AuthorArchive
